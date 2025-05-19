e50187d7039453e08afdf423370b235566c68e65
Jamie Dhonau <jldhonau@gmail.com>
2025-05-18 21:12:13 -0400

    added squareJD.ico and updated eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    "parser": "@typescript-eslint/parser",
+    "extends": [
+      "plugin:@typescript-eslint/recommended"
+    ],
+    "rules": {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars-experimental": "error",
+      "no-unused-vars": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
